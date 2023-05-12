416da9eba20f907d1546752e744dd6d37663653f
deblur99 <deblur99@kakao.com>
2023-05-13 01:02:14 +0900

    reminding flutter knowledge

--- push_script.sh
+++ push_script.sh
@@ -1,26 +1,26 @@
 # 사용법: sh -r push_script <커밋 메시지>
 # 커밋 메시지에 공백이 있을 경우, 반드시 " "를 양옆에 붙인다.
 
-echo \nexecuting git add .
+echo "\n"executing git add .
 git add .
 
-echo \ngit commit -m "$1"
+echo "\n"git commit -m "$1"
 git commit -m "$1"
 
-echo \ngit remote remove origin
+echo "\n"git remote remove origin
 git remote remove origin
 
-echo \ngit remote add origin https://github.com/busAlarm/capstonefrontendmonitor
+echo "\n"git remote add origin https://github.com/busAlarm/capstonefrontendmonitor
 git remote add origin https://github.com/busAlarm/capstonefrontendmonitor
 
-echo \ngit push origin main
+echo "\n"git push origin main
 git push origin main
 
-echo \ngit remote remove origin
+echo "\n"git remote remove origin
 git remote remove origin
 
-echo \ngit remote add origin https://github.com/OSS-util-project/Frontend
+echo "\n"git remote add origin https://github.com/OSS-util-project/Frontend
 git remote add origin https://github.com/OSS-util-project/Frontend
 
-echo \ngit push origin main
+echo "\n"git push origin main
 git push origin main
