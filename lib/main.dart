dbb7a8da523dd6277847b1d92e1c0755ad0f3cef
deblur99 <deblur99@kakao.com>
2023-05-27 19:26:44 +0900

    ongoing to implement user screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:monitor_app/utils/constants.dart';
 import 'package:monitor_app/view/splash_screen.dart';
+import 'package:monitor_app/view/user_view/user_main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: const Color(ACCENT_COLOR)),
-        title: "Monitor App",
-        home: const SplashScreen());
+      theme: ThemeData(primaryColor: const Color(ACCENT_COLOR)),
+      title: "Monitor App",
+      home: const UserMainScreen(),
+    );
+    // home: const SplashScreen());
   }
 }
